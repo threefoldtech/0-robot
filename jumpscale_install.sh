c3943e799208fbefcc41ced2c6173f82f19bc848
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-11-26 17:28:42 +0400

    update travis to try to install jumpscale

---
+++ jumpscale_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install ays9 in a docker contianer using bash installers
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
+
+curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -E bash /tmp/install.sh
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZCodeGetJS"
+eval $(ssh-agent)
+ssh-add
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZInstall_host_js9_full"
