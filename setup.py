d9e923b01d1c446faf348d893993155a84203b9b
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-12-06 15:11:02 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ZeroRobot',
+    version='0.0.1',
+    description='Automation framework for cloud workloads',
+    url='https://github.com/Jumpscale/zerorobot',
+    author='GreenItGlobe',
+    author_email='info@gig.tech',
+    license='Apache',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'JumpScale9>=9.2.0',
+        'JumpScale9Lib>=9.2.0',
+        'gevent>=1.2.2',
+        'Flask==0.10.1',
+        'Flask-Inputs==0.2.0',
+        'Jinja2==2.8',
+        'MarkupSafe==0.23',
+        'Werkzeug==0.11.4',
+        'itsdangerous==0.24',
+        'jsonschema==2.5.1',
+        'six==1.10.0',
+        'python-jose==1.3.2',
+
+    ]
+)
