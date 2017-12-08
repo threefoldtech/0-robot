c2f0dba5679694f3b06590b089f0633efd624531
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-12-08 11:10:00 +0100

    Change interface of the ZeroRobotClient

--- zerorobot/dsl/ZeroRobotClient.py
+++ zerorobot/dsl/ZeroRobotClient.py
@@ -14,14 +14,24 @@ class TemplateNotFoundError(Exception):
     pass
 
 
-class ZeroRobotClient:
+class ServicesMgr:
 
-    def __init__(self, base_url):
-        self._client = Client(base_url)
-        self._services = None
+    def __init__(self, robot):
+        self._robot = robot
+        self._client = robot._client
+
+    def _instantiate(self, data):
+        srv = ServiceProxy(data.name, data.guid, self._client)
+        if data.parent:
+            srv.parent = self._get(data.parent)
+        return srv
+
+    def _get(self, guid=None):
+        resp = self._client.api.services.GetService(guid)
+        return self._instantiate(resp.data)
 
     @property
-    def services(self):
+    def names(self):
         """
         Return a dictionnary that contains all the service present on
         the ZeroRobot
@@ -29,15 +39,30 @@ class ZeroRobotClient:
         key is the name of the service
         value is a ServiceProxy object
         """
-        if self._services is None:
-            self._services = {}
-            resp = self._client.api.services.listServices()
-            for service in resp.data:
-                service = ServiceProxy(service.name, service.guid, self._client)
-                self._services[service.name] = service
-        return self._services
+        services = {}
+        resp = self._client.api.services.listServices()
+        for service in resp.data:
+            srv = self._instantiate(service)
+            services[srv.name] = srv
+        return services
 
-    def create_service(self, template_uid, service_name, data=None):
+    @property
+    def guids(self):
+        """
+        Return a dictionnary that contains all the service present on
+        the ZeroRobot
+
+        key is the guid of the service
+        value is a ServiceProxy object
+        """
+        services = {}
+        resp = self._client.api.services.listServices()
+        for service in resp.data:
+            srv = self._instantiate(service)
+            services[srv.guid] = srv
+        return services
+
+    def create(self, template_uid, service_name, data=None):
         """
         Instantiate a service from a template
 
@@ -45,7 +70,7 @@ class ZeroRobotClient:
         @param service_name: name of the service, needs to be unique within the robot instance
         @param data: a dictionnary with the data of the service to create
         """
-        if template_uid not in self.templates:
+        if template_uid not in self._robot.templates.uids:
             raise TemplateNotFoundError("template %s not found" % template_uid)
 
         req = {
@@ -63,15 +88,27 @@ class ZeroRobotClient:
             raise err
 
         service = ServiceProxy(service_name, resp.data.guid, self._client)
-        if self._services is None:
-            self._services = {}
-        self._services[service.name] = service
         return service
 
+
+class TemplatesMgr:
+
+    def __init__(self, robot):
+        self._robot = robot
+        self._client = robot._client
+
     @property
-    def templates(self):
+    def uids(self):
         """
         Returns a list of template UID present on the ZeroRobot
         """
         resp = self._client.api.templates.ListTemplates()
         return [t.uid for t in resp.data]
+
+
+class ZeroRobotClient:
+
+    def __init__(self, base_url):
+        self._client = Client(base_url)
+        self.services = ServicesMgr(self)
+        self.templates = TemplatesMgr(self)
