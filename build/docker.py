6aa709e2e07946932a6d6da9133ad34344eadec1
Geert Audenaert <geert@mobicage.com>
2018-02-08 16:40:26 +0100

    Added docker build script

---
+++ build/docker.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python3
+"Docker image build script for 0-robot"
+from js9 import j
+
+
+REPOSITORY = "jumpscale/0-robot"
+
+
+def _install_js(prefab, branch):
+    prefab.js9.js9core.install(branch=branch)
+    for component in ("core9", "lib9", "prefab9"):
+        cmd = "cd /opt/code/github/jumpscale/%s; pip install ." % component
+        prefab.core.run(cmd)
+
+
+def _install_zrobot(prefab, branch):
+    path = prefab.tools.git.pullRepo("https://github.com/Jumpscale/0-robot.git")
+    cmds = (
+        "cd %s; git checkout %s" % (path, branch),
+        "cd %s; pip install -r requirements.txt" % path,
+        "cd %s; pip install ." % path,
+    )
+    for cmd in cmds:
+        prefab.core.run(cmd)
+
+
+def build_docker(tag, jsbranch, zrbranch, push):
+    "Build and push docker image for 0-robot"
+    print("Starting docker container ... ", end='')
+    container = j.sal.docker.client.containers.create("ubuntu:16.04", command="sleep 3600")
+    container.start()
+    print("done!\nEstablishing prefab connection ... ", end='')
+    try:
+        ex = j.tools.executor.getLocalDocker(container.id)
+        prefab = j.tools.prefab.get(executor=ex)
+        print("done!\nUpdating ubuntu apt definitions ... ", end='')
+        prefab.system.package.mdupdate()
+        print("done!\nInstalling python3-dev, git & curl ... ", end='')
+        prefab.system.package.install("python3-dev,git,curl")
+        print("done!\nInstalling jumpscale ... ", end='')
+        _install_js(prefab, jsbranch)
+        print("done!\nInstalling 0-robot ... ", end='')
+        _install_zrobot(prefab, zrbranch)
+        print("done!\nCommiting 0-robot docker image ... ", end='')
+        container.commit("jumpscale/0-robot", tag)
+        if push:
+            print("done!\nPushing docker image ... ")
+            j.sal.docker.client.images.push("jumpscale/0-robot", tag)
+            print("0-robot build and published successfully!")
+        else:
+            print("done!\n0-robot build successfully!")
+    finally:
+        container.stop()
+        container.remove()
+
+
+def _main():
+    import argparse
+    parser = argparse.ArgumentParser("Build and push docker image for 0-robot")
+    parser.add_argument("--tag", type=str, default="latest", help="Version tag for docker image")
+    parser.add_argument("--jsbranch", type=str, default="development",
+                        help="Jumpscale git branch, tag or revision to build")
+    parser.add_argument("--zrbranch", type=str, default="master",
+                        help="0-robot git branch, tag or revision to build")
+    parser.add_argument("--push", help="Push to docker hub")
+    args = parser.parse_args()
+    build_docker(args.tag, args.jsbranch, args.zrbranch, args.push)
+
+
+if __name__ == "__main__":
+    _main()
