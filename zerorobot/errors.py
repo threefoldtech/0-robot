2f396e473d1725d3055bdd718c4b389096ffaa06
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2018-03-12 17:39:23 +0100

    Updates ExpectedError docstring

--- zerorobot/errors.py
+++ zerorobot/errors.py
@@ -2,5 +2,6 @@ class ExpectedError:
     """
     Defines an exception base for expected 0-robot errors
     This allows us to detect expected exceptions.
+    Make the error object inherit this class to mark it as an expected error.
     """
     pass
