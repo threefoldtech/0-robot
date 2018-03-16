db0bc8d4d646d840545e395b26bbec1837810132
Geert Audenaert <geert@mobicage.com>
2018-03-16 20:05:23 +0100

    Hardened js9_config init in dockerentrypoint

--- utils/scripts/packages/dockerentrypoint.py
+++ utils/scripts/packages/dockerentrypoint.py
@@ -3,24 +3,36 @@ import os
 import time
 from js9 import j
 
+STANDARD_CONFIG_DIR = '/opt/code/local/stdorg/config'
+
 if not j.sal.fs.exists("/root/.ssh/id_rsa"):
     j.tools.prefab.local.system.ssh.keygen(user='root', name='id_rsa')
+    pubkey = j.sal.fs.readFile("/root/.ssh/id_rsa.pub")
     print("Configure the following ssh public key to have write access to your config / data repository:\n%s" % pubkey)
     exit(0)
 
-j.tools.prefab.local.core.run("js9_config init -s -k /root/.ssh/id_rsa")
+config_repo = os.environ.get("config_repo")
+if config_repo:
+    path = j.tools.prefab.local.tools.git.pullRepo(config_repo)
+else:
+    j.sal.fs.createDir(STANDARD_CONFIG_DIR)
+    path = STANDARD_CONFIG_DIR
+    if not j.sal.fs.exists("%s/.git" % STANDARD_CONFIG_DIR):
+        j.tools.prefab.local.core.run("cd %s && git init ." % STANDARD_CONFIG_DIR)
+
+j.tools.prefab.local.core.run("js9_config init -s -k /root/.ssh/id_rsa -p %s" % path)
 
 cmd_line = "zrobot server start"
 args = [
     "listen", 
-    "data-repo", 
-    "template-repo", 
-    "config-repo", 
+    "data_repo", 
+    "template_repo", 
+    "config_repo", 
     "debug", 
-    "telegram-bot-token", 
-    "telegram-chat-id", 
-    "auto-push", 
-    "auto-push-interval"
+    "telegram_bot_token", 
+    "telegram_chat_id", 
+    "auto_push", 
+    "auto_push_interval"
 ]
 for arg in args:
     val = os.environ.get(arg)
@@ -28,6 +40,6 @@ for arg in args:
         if arg in ("debug", "auto-push"):
             cmd_line += " --%s" % arg
         else:
-            cmd_line += " --%s '%s'" % (arg, val.replace("'", "\\'"))
+            cmd_line += " --%s '%s'" % (arg.replace("_", "-") , val.replace("'", "\\'"))
 
 j.tools.prefab.local.core.run(cmd_line)
