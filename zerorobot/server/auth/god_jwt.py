851b00e476ca05c5d9afe40bfd3d2506a98fd452
Jan DeLandt <jan@bettertoken.com>
2018-08-10 10:58:51 +0100

    move logger out of global declaration (dependency issue)

--- zerorobot/server/auth/god_jwt.py
+++ zerorobot/server/auth/god_jwt.py
@@ -5,7 +5,6 @@ from zerorobot import config
 from jose import jwt
 from jumpscale import j
 
-logger = j.logger.get('zrobot')
 _token_prefix = "Bearer "
 
 
@@ -32,6 +31,7 @@ def verify(token):
         if claims == expected:
             return True
     except Exception as err:
+        logger = j.logger.get('zrobot')
         logger.error('error decoding god token: %s', str(err))
 
     return False
