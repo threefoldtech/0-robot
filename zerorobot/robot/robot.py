a3ae33f5c38b38796e01e6212e8025ffc0ede397
Ahmed T. Youssef <xmonader@gmail.com>
2018-10-08 14:13:36 +0200

    Allow 0-robot to start with zdb backend for configmanager  (#41)
    * configmanager for 0-robot
    
    * use _parse_zdb from data_repo module
    
    * harden zrobot command and robot.py initialization based on namespace too
    
    * don't patch too early
    
    * maintain the readability of start cli command
    
    * ensure the robot has a namespace when zdb backend is used

--- zerorobot/robot/robot.py
+++ zerorobot/robot/robot.py
@@ -97,10 +97,8 @@ class Robot:
 
         config.mode = mode
         config.god = god  # when true, this allow to get data and logs from services using the REST API
-
         if config.data_repo is None:
             raise RuntimeError("Not data repository set. Robot doesn't know where to save data.")
-
         if not j.tools.configmanager.path:
             raise RuntimeError("config manager is not configured, can't continue")
 
@@ -110,7 +108,10 @@ class Robot:
         logger = j.logger.get('zerorobot')
         logger.info("data directory: %s" % config.data_repo.path)
         logger.info("config directory: %s" % j.tools.configmanager.path)
-        logger.info("sshkey used: %s" % os.path.expanduser(os.path.join('~/.ssh', j.tools.configmanager.keyname)))
+        keypath = j.tools.configmanager.keypath
+        if not keypath:
+            keypath = os.path.expanduser(os.path.join('~/.ssh', j.tools.configmanager.keyname))
+        logger.info("sshkey used: %s" % keypath)
 
         # configure prometheus monitoring
         if not kwargs.get('testing', False):
