e340530e52793cf7fc7d6aeb5983d942f0ec0908
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-11-09 10:41:05 +0100

    update symlink release script

--- utils/symlink_release.py
+++ utils/symlink_release.py
@@ -1,25 +1,37 @@
 from jumpscale import j
 
-FLIST_HASH = '679d829a0d'
-VERSION = '0.7.2'
+FLIST_HASH = '2a8acdc52c'
+VERSION = '0.9.0_rc1'
 
 
 iyo = j.clients.itsyouonline.get()
-jwt = iyo.jwt_get(scope='user:memberof:gig-official-apps')
+jwt = iyo.jwt_get(scope='user:memberof:tf-official-apps')
 data = {
     'token_': jwt,
-    'username': 'gig-official-apps',
-    'url': 'https://hub.gig.tech/api',
+    'username': 'tf-official-apps',
+    'url': 'https://hub.grid.tf/api',
 }
 
-j.clients.zerohub.delete('official-apps')
-hub = j.clients.zerohub.get('official-apps', data=data, interactive=False)
+j.clients.zhub.delete('official-apps')
+hub = j.clients.zhub.get('official-apps', data=data, interactive=False)
 hub.authenticate()
-hub.api.set_user('gig-official-apps')
+hub.api.set_user('tf-official-apps')
 
+source = 'threefoldtech-0-robot-master-%s.flist' % FLIST_HASH
+destination = 'threefoldtech-0-robot-%s.flist' % VERSION
+print('promote %s to %s' % (source, destination))
+hub.promote('tf-autobuilder', source, destination)
 
-hub.promote('gig-autobuilder', 'threefoldtech-0-robot-master-%s.flist' % FLIST_HASH, 'threefoldtech-0-robot-%s.flist' % VERSION)
-hub.promote('gig-autobuilder', 'threefoldtech-0-robot-autostart-master-%s.flist' % FLIST_HASH, 'threefoldtech-0-robot-autostart-%s.flist' % VERSION)
+source = 'threefoldtech-0-robot-autostart-master-%s.flist' % FLIST_HASH
+destination = 'threefoldtech-0-robot-autostart-%s.flist' % VERSION
+print('promote %s to %s' % (source, destination))
+hub.promote('tf-autobuilder', source, destination)
 
-hub.symlink('threefoldtech-0-robot-%s.flist' % VERSION, 'threefoldtech-0-robot-latest.flist')
-hub.symlink('threefoldtech-0-robot-autostart-%s.flist' % VERSION, 'threefoldtech-0-robot-autostart-latest.flist')
+source = 'threefoldtech-0-robot-%s.flist' % VERSION
+destination = 'threefoldtech-0-robot-latest.flist'
+hub.symlink(source, destination)
+
+source = 'threefoldtech-0-robot-autostart-%s.flist' % VERSION
+destination = 'threefoldtech-0-robot-autostart-latest.flist'
+print('symlink %s to %s' % (source, destination))
+hub.symlink(source, destination)
