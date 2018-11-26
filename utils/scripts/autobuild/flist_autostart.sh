5cd9d3fd152deeebd04bd485c27a7a9f2f5f6db2
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-11-26 13:57:03 +0100

    add ipmitool to 0-robot flist

--- utils/scripts/autobuild/flist_autostart.sh
+++ utils/scripts/autobuild/flist_autostart.sh
@@ -8,7 +8,7 @@ mkdir -p $ARCHIVE
 
 # install system deps
 apt-get update
-apt-get install -y locales git sudo python3-pip libffi-dev python3-dev libssl-dev libpython3-dev libssh-dev libsnappy-dev build-essential pkg-config libvirt-dev libsqlite3-dev -y
+apt-get install -y locales git sudo python3-pip libffi-dev python3-dev libssl-dev libpython3-dev libssh-dev libsnappy-dev build-essential pkg-config libvirt-dev libsqlite3-dev ipmitool -y
 
 # setting up locales
 if ! grep -q ^en_US /etc/locale.gen; then
