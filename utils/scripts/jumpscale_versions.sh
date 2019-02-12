b0924432e621c46463a68db2a78622a3e8f1243c
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-02-12 14:00:07 +0100

    upgrade six package to version 1.12.0

--- utils/scripts/jumpscale_versions.sh
+++ utils/scripts/jumpscale_versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export jumpscale_core_revision="development"
+export jumpscale_core_revision="six_version"
 export jumpscale_lib_revision="development"
 export jumpscale_prefab_revision="development"
